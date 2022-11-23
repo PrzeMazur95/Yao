d60e6646704ea02dbb9f633272f70362e9bf01a5
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-11-23 23:32:19 +0100

    Add ManagerRegistry to be able to add users to db

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\UserRepository;
 use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
 
 class UserController extends AbstractController
 {
