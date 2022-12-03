b1d5c48e87761657ef237c01aa3dee2bb4453ee3
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-12-03 19:39:41 +0100

    Use annotations instead of yaml routes

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RegistrationController extends AbstractController
 {
+    #[Route('/registration', name: 'registration')]
     public function index(): Response
     {
         return $this->render(
