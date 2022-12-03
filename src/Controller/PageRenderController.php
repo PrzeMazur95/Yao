40d5e907a873768966ff57075a3af286f7b348a4
PrzeMazur95 <mazurkiewiczprzemyslaw95@gmail.com>
2022-12-03 19:45:53 +0100

    Use annotations instead of yaml routes

--- src/Controller/PageRenderController.php
+++ src/Controller/PageRenderController.php
@@ -3,10 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PageRenderController extends AbstractController
 {
+    #[Route('/index', name: 'index')]
     public function index(): Response
     {
         return $this->render(
